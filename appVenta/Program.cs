3b6fc3d5cacb91d0f88200cfa55a0b2a258e2e36
denis lopez <69539735+Lopez1710@users.noreply.github.com>
2021-05-21 21:42:28 -0600

    para evitar mas errores

--- appVenta/Program.cs
+++ appVenta/Program.cs
@@ -17,7 +17,7 @@ namespace appVenta
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmPadre());
         }
     }
 }
